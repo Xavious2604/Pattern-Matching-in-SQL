76c26d57336b92a138bed21b3c78430f4c338cef
Mohammed Irfan Shaikh <141063158+Xavious2604@users.noreply.github.com>
2024-07-14 20:25:00 +0530

    Create MyCode.sql

---
+++ MyCode.sql
@@ -0,0 +1,13 @@
+SELECT offices, customers FROM mysqlsampledatabase WHERE city LIKE 'Pr%';  
+
+SELECT * FROM employees WHERE email LIKE '%@%';  
+
+SELECT * FROM customers WHERE city LIKE '%i';
+
+SELECT * FROM offices WHERE postalCode LIKE '2%'; 
+
+SELECT * FROM products WHERE productScale LIKE '%50%'
+
+SELECT * FROM customers WHERE city LIKE 'Na_ik'; 
+
+SELECT * FROM products WHERE MSRP LIKE '3__00';  
